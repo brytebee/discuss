734c815b87126634199d3c9f148860366ed09706
Bright Atsighi <brytebee@gmail.com>
2024-03-06 10:43:58 +0100

    Rendered the slug to page

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
-type Props = {};
+type Props = {
+  params: {
+    slug: string;
+  };
+};
 
-export default function TopicShow(props: Props) {
-  return <div>topic show page</div>;
+export default function TopicShow({ params: { slug } }: Props) {
+  return (
+    <div className="grid grid-cols-4 gap-4 p-4">
+      <div className="cols-span-3">
+        <h1 className="text-2xl font-bold mb-2">{slug}</h1>
+      </div>
+      <div></div>
+    </div>
+  );
 }
